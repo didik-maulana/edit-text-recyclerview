80adf0684e9ff169e11c35205cb529a82e8acb7c
Didik Maulana Ardiansyah <didikmaulana49@gmail.com>
2019-05-02 19:51:08 +0700

    add structure files

--- app/src/main/java/com/didik/learn/presentation/MainActivity.kt
+++ app/src/main/java/com/didik/learn/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.didik.learn
+package com.didik.learn.presentation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.didik.learn.R
 
 class MainActivity : AppCompatActivity() {
 
